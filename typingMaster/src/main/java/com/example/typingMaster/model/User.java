5c67bf8d4578babf5901478c53be791ea7015ab7
yashg18-bit <yashgupta.yg154@gmail.com>
2026-06-22 00:08:42 +0530

    rename user table

--- typingMaster/src/main/java/com/example/typingMaster/model/User.java
+++ typingMaster/src/main/java/com/example/typingMaster/model/User.java
@@ -1,17 +1,23 @@
 package com.example.typingMaster.model;
 
-import jakarta.persistence.*;
-
-import lombok.Getter;
-import lombok.Setter;
-
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
 @Getter
 @Setter
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
